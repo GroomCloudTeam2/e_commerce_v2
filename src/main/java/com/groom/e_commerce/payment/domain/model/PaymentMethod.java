7dd02eb0da9f549ff8a73facdd3e8e0a14fed212
yseoy0311 <yseoy0311@gmail.com>
2026-01-21 13:33:19 +0900

    init

--- src/main/java/com/groom/e_commerce/payment/domain/model/PaymentMethod.java
+++
@@ -1,9 +0,0 @@
-package com.groom.e_commerce.payment.domain.model;
-
-public enum PaymentMethod {
-	CARD,
-	EASY_PAY,
-	TRANSFER,
-	MOBILE,
-	UNKNOWN
-}
--- src/main/java/com/groom/e_commerce/payment/infrastructure/api/toss/dto/request/TossConfirmRequest.java
+++
@@ -1,8 +0,0 @@
-package com.groom.e_commerce.payment.infrastructure.api.toss.dto.request;
-
-public record TossConfirmRequest(
-	String paymentKey,
-	String orderId,
-	Long amount
-) {
-}
