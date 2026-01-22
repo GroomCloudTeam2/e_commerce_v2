45de39e90777974cb3dee272b8494d23e27ea6e2
yyytgf123 <hyunho3445@gmail.com>
2026-01-22 13:17:38 +0900

    feat: add user domain

---
+++ src/main/java/com/groom/e_commerce/user/domain/entity/user/PeriodType.java
@@ -0,0 +1,6 @@
+package com.groom.e_commerce.user.domain.entity.user;
+
+public enum PeriodType {
+	DAILY,
+	MONTHLY
+}
---
+++ src/main/java/com/groom/e_commerce/user/domain/entity/user/UserStatus.java
@@ -0,0 +1,7 @@
+package com.groom.e_commerce.user.domain.entity.user;
+
+public enum UserStatus {
+	ACTIVE,
+	BANNED,
+	WITHDRAWN
+}
