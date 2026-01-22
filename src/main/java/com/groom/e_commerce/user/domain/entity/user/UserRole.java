9aa603c8cd95487f24905346e89e3cb7ad023d22
yyytgf123 <hyunho3445@gmail.com>
2026-01-22 13:14:11 +0900

    refactor: user domain

--- src/main/java/com/groom/e_commerce/user/domain/entity/user/UserRole.java
+++
@@ -1,8 +0,0 @@
-package com.groom.e_commerce.user.domain.entity.user;
-
-public enum UserRole {
-	USER,
-	OWNER,
-	MANAGER,
-	MASTER
-}
--- src/main/java/com/groom/e_commerce/user/domain/entity/user/UserStatus.java
+++
@@ -1,7 +0,0 @@
-package com.groom.e_commerce.user.domain.entity.user;
-
-public enum UserStatus {
-	ACTIVE,
-	BANNED,
-	WITHDRAWN
-}
